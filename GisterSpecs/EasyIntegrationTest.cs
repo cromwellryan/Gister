91c6cb92387c52ade491de3faac75908672dd0a8
Ryan Cromwell <ryan@echelontouch.com>
2011-11-20 23:45:27 -0500

    added easy integration test

---
+++ GisterSpecs/EasyIntegrationTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using EchelonTouchInc.Gister.Api;
+using NUnit.Framework;
+
+namespace GisterSpecs
+{
+    [TestFixture]
+    public class EasyIntegrationTest
+    {
+        [Test]
+        public void DoWop()
+        {
+            new GistApi() { GitHubSender = new HttpGitHubSender() }.Create("wopwop.js", "dowop", "cromwellryan",
+                                                                         "ship it n0w!");
+        }
+    }
+}
