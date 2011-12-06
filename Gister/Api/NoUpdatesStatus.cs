bef192c2a26bab3a62d508b5f18b5b2157e66c64
Ryan Cromwell <ryan@echelontouch.com>
2011-12-06 13:51:01 -0500

    removd unused

--- Gister/Api/NoUpdatesStatus.cs
+++
@@ -1,9 +0,0 @@
-namespace EchelonTouchInc.Gister.Api
-{
-    public class NoUpdatesStatus : UpdatesStatus
-    {
-        public void NotifyUserThat(string messagetotelltheuser)
-        {
-        }
-    }
-}
