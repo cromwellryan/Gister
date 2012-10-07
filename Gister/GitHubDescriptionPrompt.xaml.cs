2d289a01d9456f6f2229bbd48441656bc992e3be
Ling <panan.songyu@gmail.com>
2012-10-07 23:04:50 +0800

    add new windows for description
    Signed-off-by: Ling <panan.songyu@gmail.com>

---
+++ Gister/GitHubDescriptionPrompt.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace EchelonTouchInc.Gister
+{
+    /// <summary>
+    /// Interaction logic for GitHubDescriptionPrompt.xaml
+    /// </summary>
+    public partial class GitHubDescriptionPrompt : Window
+    {
+        public GitHubDescriptionPrompt()
+        {
+            InitializeComponent();
+        }
+
+        public bool Private{get;set;}
+
+        public string Description { get; set; }
+
+        public void Prompt()
+        {
+            ShowDialog();
+        }
+
+        private void OnOkClick(object sender, RoutedEventArgs e)
+        {
+            Description = txtDescription.Text;
+            DialogResult = true;
+            Close();
+        }
+
+        private void OnCancelClick(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+
+            Close();
+        }
+
+    }
+}
